23133423c4feca7ec64467c49a30abb1aeb003ea
KARTHIK UPPULURI <koopuluri@karthiks-mbp.attlocal.net>
2016-07-20 17:25:42 -0700

    removed the vocab parameter

--- dna_sequence.py
+++ dna_sequence.py
@@ -1,16 +1,27 @@
 
-def build_state_machine(s, vocab):
+def build_state_machine(s):
     """
     State machine represented as: [{vocab_option: next_index}]
+    @param s: string to build state machine for
     """
 
     # building the options for the start state:
+
+    # possible options from each char is the unique characters within the input string:
+    vocab = set(s)
+
+    # building the start state before iteration (TODO: handle this within the loop!)
     start_options = {s[0]: 1}
     for v in vocab:
         if v != s[0]:
             start_options[v] = 0
 
+    # sm is the state machine where index corresponds to the state there, and
+    # its options and what state each goes to is represented in the options_map
+    # ({vocab_option: index_it_maps_to})
     sm = [start_options]
+
+    # going through each character of input string and adding to state machine:
     for i in xrange(len(s) - 1):
         curr_char, next_char = s[i], s[i + 1]
         options_map = {next_char: i + 2}
@@ -36,7 +47,7 @@ def build_state_machine(s, vocab):
     return sm
 
 def is_substring(needle, haystack):
-    sm = build_state_machine(needle, set(needle))
+    sm = build_state_machine(needle)
     i = 0
     for c in haystack:
         if c in sm[i]:
@@ -49,3 +60,6 @@ def is_substring(needle, haystack):
             return True
 
     return False
+
+# testing:
+print is_substring('acca', 'ajkdljsacajkljaccaaaa')
